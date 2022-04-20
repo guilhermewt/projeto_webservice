8cbb795412860645a7ba75bdd120f669dd023016
guilherme silva <welliston10@gmail.com>
2022-04-20 16:03:18 -0300

    delete, update, findById

--- src/main/java/com/webserviceproject/entities/controllers/UsuarioController.java
+++ src/main/java/com/webserviceproject/entities/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,10 +27,29 @@ public class UsuarioController {
 		return ResponseEntity.ok().body(list);
 	}
 	
+   @RequestMapping(value = "/{id}")	
+   public ResponseEntity<Usuario> findById(@PathVariable long id){
+	   Usuario user = service.findById(id);
+	   return ResponseEntity.ok().body(user);
+   }
+	
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Usuario> insert(@RequestBody Usuario user){
 		Usuario obj = service.insert(user);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Usuario> update(@PathVariable long id, @RequestBody Usuario obj){
+		obj.setId(id);
+		Usuario updateObj = service.update(obj);
+		return ResponseEntity.ok().body(updateObj);
 	}
 
 }
--- src/main/java/com/webserviceproject/services/UsuarioService.java
+++ src/main/java/com/webserviceproject/services/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.webserviceproject.services;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.ObjDoubleConsumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,7 +20,30 @@ public class UsuarioService {
 		return repositorio.findAll();
 	}
 	
+	public Usuario findById(long id) {
+		Optional<Usuario> obj =  repositorio.findById(id);
+		return obj.get();
+	}
+	
 	public Usuario insert(Usuario obj) {
 		return repositorio.save(obj);
+	}
+	
+	public void delete(long id) {
+		 repositorio.deleteById(id);
+	}
+	
+	public Usuario update(Usuario obj) {
+		Usuario user = repositorio.getOne(obj.getId());
+		atualizarUsuario(user,obj);
+		return repositorio.save(user);
+	}
+
+	private void atualizarUsuario(Usuario user, Usuario obj) {
+		user.setNome(obj.getNome());
+		user.setEmail(obj.getEmail());
+		user.setTelefone(obj.getTelefone());
+		user.setSenha(obj.getSenha());
+		
 	}
 }
